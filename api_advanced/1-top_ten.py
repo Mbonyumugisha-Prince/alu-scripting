20793681e7cce4e73bce7e56113c4a64ecb9e2c2
Mbonyumugisha-Prince <p.mbonyumug@alustudent.com>
2025-02-21 17:33:21 +0200

    commit

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,39 +1,31 @@
 #!/usr/bin/python3
-"""
-Function that queries the Reddit API and prints the titles
-of the first 10 hot posts listed for a given subreddit.
-"""
-import json
+"""Module for querying Reddit API"""
 import requests
-import sys
 
 
 def top_ten(subreddit):
-    """Get the top ten hot posts of a subreddit
+    """Queries the Reddit API and prints titles of first 10 hot posts
+    for a given subreddit.
+
     Args:
-        subreddit: the subreddit to search
-    Returns:
-        None if subreddit is invalid, otherwise prints the titles
+        subreddit: name of the subreddit to query
     """
-    # Set custom User-Agent to avoid rate limiting
-    headers = {'User-Agent': 'MyAPI/0.0.1'}
-    
-    # Reddit API URL
-    url = f'https://www.reddit.com/r/{subreddit}/hot.json?limit=10'
-    
-    try:
-        # Make request and set allow_redirects to False
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        
-        # Check if response is successful and not a redirect
-        if response.status_code != 200:
-            print(None)
-            return
-            
-        # Parse response and print titles
-        data = response.json().get('data', {}).get('children', [])
-        for post in data:
-            print(post.get('data', {}).get('title'))
-            
-    except Exception:
+    url = 'https://www.reddit.com/r/{}/hot/.json'.format(subreddit)
+    headers = {'User-Agent': 'Custom/0.0.1'}
+    params = {'limit': 10}
+
+    response = requests.get(url, headers=headers, params=params,
+                          allow_redirects=False)
+
+    if response.status_code != 200:
         print(None)
+        return
+
+    results = response.json().get('data', {}).get('children', [])
+
+    if not results:
+        print(None)
+        return
+
+    for post in results:
+        print(post.get('data', {}).get('title'))
